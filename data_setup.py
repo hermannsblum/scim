b043acebdab20a28b1f77d126cf25d4ccbcabfb0
Hermann <blumh@ethz.ch>
2021-06-15 09:43:43 +0200

    add more models to download

--- data_setup.py
+++ data_setup.py
@@ -9,3 +9,9 @@ tfds.load('nyu_depth_v2_labeled/labeled')
 tfds.load('coco_segmentation')
 
 load_gdrive_file('15vSz0eBSjN4K1b3DvKZ1PQTtW4cY-_2W', ending='pth')
+load_gdrive_file('1zLhaoGjnU5nDyAfcBnPss6NFBKuo9iuE', ending='pth')
+load_gdrive_file('1yb7izcZyyjL3Bm-Y1vNoUyHBWOhqUen_', ending='pth')
+load_gdrive_file('1QvES0v_oHTgmx0sxZsAjvCWQvxS3Z6DO', ending='pth')
+load_gdrive_file('1EnMk9YWPGET2sBBgjni7deadYOYPqn0O', ending='pth')
+load_gdrive_file('1Fk9efJuv07bnNy91l1xzjEFtQ_qyyufR', ending='pth')
+
