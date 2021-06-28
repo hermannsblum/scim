d7f4b05403e0007ff7ffc082cc199fcfb5940110
Hermann <blumh@ethz.ch>
2021-06-28 15:06:47 +0200

    add incense loader

--- semseg_density/sacred_utils.py
+++ semseg_density/sacred_utils.py
@@ -1,4 +1,5 @@
 from sacred.observers import MongoObserver, FileStorageObserver
+import incense
 import semseg_density.settings as settings
 
 
@@ -16,3 +17,16 @@ def get_observer():
     return FileStorageObserver.create(settings.EXPERIMENT_STORAGE_FOLDER)
   else:
     raise UserWarning("No observer settings found.")
+
+
+def get_incense_loader():
+  if hasattr(settings, 'EXPERIMENT_DB_HOST') and settings.EXPERIMENT_DB_HOST:
+    mongouri = 'mongodb://{user}:{pwd}@{host}/{db}'.format(
+        host=settings.EXPERIMENT_DB_HOST,
+        user=settings.EXPERIMENT_DB_USER,
+        pwd=settings.EXPERIMENT_DB_PWD,
+        db=settings.EXPERIMENT_DB_NAME)
+    return incense.ExperimentLoader(mongo_uri=mongouri,
+                                    db_name=settings.EXPERIMENT_DB_NAME)
+  else:
+    raise UserWarning("No loader settings found.")
